08e9dda1d4131f6f95215422742a89e6154da706
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-25 10:33:51 +0100

    add tags

--- tools/create_csv.py
+++ tools/create_csv.py
@@ -12,7 +12,10 @@ with open('netbsd.csv', mode='w') as netbsd_f:
                                quoting=csv.QUOTE_MINIMAL)
     #version,codename,series,created,release,eol
     netbsd_writer.writerow(['version', 'codename','branch', 'series', 'created', 'release', 'eos'])
+    netbsd_writer.writerow(['8.0', 'netbsd-8', 'stable', '', '20-05-1993', '', ''])
+    netbsd_writer.writerow(['8.0', 'netbsd-8', 'stable', '', '20-05-1993', '', ''])
+    netbsd_writer.writerow(['8.0', 'netbsd-8', 'stable', '', '20-05-1994', '', ''])
     netbsd_writer.writerow(['1.2.1', 'netbsd-1', 'maintenence/security', '', '20-05-1997', 'NetBSD', ''])
     netbsd_writer.writerow(['2.0.1', 'netbsd-1', 'maintenence/security', '', '20-05-1997', 'NetBSD', ''])
     netbsd_writer.writerow(['2.1', 'netbsd-2', 'stable', '', '02-11-2005', '', '21-08-2008'])
-    netbsd_writer.writerow(['8.0', 'netbsd-8', 'stable', '', '20-05-1993', ''])
+    
