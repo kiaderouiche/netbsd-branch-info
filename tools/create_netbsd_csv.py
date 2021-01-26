cf78605541398e3ce1b6c33d2ed53e81224f9084
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-27 00:50:45 +0100

    prepare csv data for new realease

--- tools/create_netbsd_csv.py
+++ tools/create_netbsd_csv.py
@@ -13,11 +13,11 @@ with open('netbsd.csv', mode='w') as netbsd_f:
     netbsd_writer = csv.writer(netbsd_f, delimiter=',', quotechar='"',
                                quoting=csv.QUOTE_MINIMAL)
     #version,branch,codename, series, created,release,eor
-    netbsd_writer.writerow(['version', 'branch','codename', 'series', 'created', 'release', 'eor'])
-    netbsd_writer.writerow(['0.8', 'netbsd-0', 'stable', '', '20-05-1993', 'major', 'death'])
-    netbsd_writer.writerow(['0.9', 'netbsd-0', 'stable', '', '23-08-1993', 'major', 'death'])
-    netbsd_writer.writerow(['1.0', 'netbsd-1', 'stable', '', '26-10-1993', 'major', 'death'])
-    netbsd_writer.writerow(['1.2.1', 'netbsd-1.2', 'security', '', '20-05-1997', 'minor', ''])
+    netbsd_writer.writerow(['version', 'branch', 'series', 'release', 'eor'])
+    netbsd_writer.writerow(['0.8', 'netbsd-0', 'major', '20-05-1993', 'major', 'death'])
+    netbsd_writer.writerow(['0.9', 'netbsd-0', 'major', '', '23-08-1993', 'major', 'death'])
+    netbsd_writer.writerow(['1.0', 'netbsd-1', 'major', '', '26-10-1993', 'major', 'death'])
+    netbsd_writer.writerow(['1.2.1', 'netbsd-1.2', 'minor', '', '20-05-1997', 'minor', ''])
     netbsd_writer.writerow(['2.0.1', 'netbsd-2', 'security', '', '20-05-1997', 'minor', ''])
     netbsd_writer.writerow(['2.1', 'netbsd-2.1', 'stable', '', '02-11-2005', 'minor', '21-08-2008'])
     netbsd_writer.writerow(['6.0', 'netbsd-6', 'stable', '', '22-09-2014', 'major', '21-08-2008'])
